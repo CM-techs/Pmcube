df63e637e7f5fc882c3e1c0d4bf74369b2691789
CM-techs <cchellamani12@gmail.com>
2026-04-03 12:54:11 +0530

    updated UI and fixed errors

---
+++ src/About.jsx
@@ -0,0 +1,29 @@
+function About (){
+    return<>
+   
+    <div className="a-img-box">
+        <img className="ab-img" 
+        src="https://static.vecteezy.com/system/resources/previews/007/213/472/non_2x/a-team-discussion-on-office-table-teamwork-character-illustration-business-character-illustration-on-white-background-vector.jpg"/>
+        </div>
+        <div className="a-tex-box"> 
+            <h4>🔹 Who We Are</h4>
+        <p>We are a team of creative thinkers and problem solvers focused on building meaningful experiences.
+             With a strong foundation in trust, quality, and commitment, we aim to provide reliable services that make a difference.</p>
+        <h4>🔹 About Us</h4>
+        <p>We are committed to delivering quality services with dedication and professionalism. 
+            Our team works with passion and innovation to create solutions that help our clients grow and succeed.
+             Customer satisfaction and excellence remain at the heart of everything we do.</p>
+        </div>
+        <div className="a-tex-box2">
+            <h4>Our Speciality :</h4>
+            <ul type="none">
+            <li>🔹 30Days Free Demo Classes</li>
+            <li>🔹 Free Consulting</li>
+            </ul>
+            </div>
+            <h2 className="a-tag-tex">Learn Today. Lead Tomorrow</h2>
+    </>
+}export default About
+
+
+
